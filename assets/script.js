d4339d7e53f855dd40dbb6b882777b78f4701c9e
SidheLore <103224878+SidheLore@users.noreply.github.com>
2022-09-22 15:28:57 -0400

    javascript code added, variables declared, confirm messages

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,51 @@
 // Assignment code here
 
-// Variables declared.
+// Variables declared
     var charUppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     var charLowerercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     var charNumeric = ['0', '1', '2', '3', '4', '5', '6' ,'7', '8', '9'];
     var charSpecial = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '~', '[', ']', '{', '}', '/', '<', '>',];
-
+    var charSet = "";
+    var password="";
+    var passwordLength;
+    var passwordSelect;
 
 //
 function generatePassword(){
-
-}
-//
-if (charLength < 8 || charLength > 128){
-  window.alert("Password must be at least 8 characters and no more than 128 characters. Please try again.");
-}
-else if (charLength >= 8 && charLength <= 128) {
+  var passwordLength = prompt("What would you like the character length of your password to be? Please enter a value between 8 and 128.");
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Password must be at least 8 characters and no more than 128 characters. Please try again.");
+    return null;
+  }
+  while (charLength >= 8 && charLength <= 128) {
     return password;
+  }
+
+ if (!charUppercase && !charLowercase && !charNumeric && !charSpecial){
+   window.alert("Please choose at least one character type.");
+    return;
+ }
+ else if (){
+
+  }
+ else if (){
+
+  else if(){
+
+  }
+
+  charUppercase = confirm("Would you like to include uppercase letters")
+  charLowercase = confirm("Would you like to include lowercase letters?")
+  charNumeric = confirm("Would you like to include numbers?")
+  charSpecial = confirm("Would you like to include special characters?")
 }
 
-
-
-
+// Generate random password
+for (var i = 0; i < charLength; i++){
+  password += charSet[Math.floor(Math.random() * charSet.length)];
+console.log(password);
+}
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -33,7 +57,9 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+ 
